518b84d6caef28c442c7ed552e479e46f87bf42a
angelvinyals <angel.vinyals@gmail.com>
2017-11-27 19:15:30 +0100

    NoResult Component
    No Result Component. List a diferent component when SearchResult give  No tracks.

---
+++ src/components/NoResults/NoResults.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './NoResults.css';
+
+
+class NoResults extends Component {
+	
+		
+  	render() {
+    	return (
+      		<div className="SearchResults">
+		  		<h2> NO Results</h2>		  				  		
+			</div>
+    	);
+  	}
+}
+
+export default NoResults;
