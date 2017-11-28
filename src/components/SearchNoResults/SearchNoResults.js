eb6f0c1a702e63a3067df9f23af92987d1e8a879
angelvinyals <angel.vinyals@gmail.com>
2017-11-28 12:54:16 +0100

    add SearchNoResults component
    Add SearchNoResult Component
    Delete NoResults Component

---
+++ src/components/SearchNoResults/SearchNoResults.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './SearchNoResults.css';
+
+
+class SearchNoResults extends Component {
+  
+    
+    render() {
+      return (
+          <div className="SearchNoResults">
+          <h2>Results</h2>
+          No tracks
+          
+      </div>
+      );
+    }
+}
+
+export default SearchNoResults;
